1d872411b0727a50c280a40f8931db47d3c62354
Germán Cascales <germancascales@protonmail.ch>
2015-03-28 01:16:13 +0100

    Reorganización JavaScript

---
+++ megastar/static/js/dialog.js
@@ -0,0 +1,54 @@
+$(function () {
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "no-close",
+        maxHeight: 600,
+        show: {
+            effect: "puff",
+            duration: 500
+        },
+        hide: {
+            effect: "drop",
+            duration: 500
+        }
+    });
+    
+    $("#dialogRadio").dialog({
+        autoOpen: false,
+        modal: true,
+        dialogClass: "no-close",
+        maxHeight: 600,
+        show: {
+            effect: "puff",
+            duration: 500
+        },
+        hide: {
+            effect: "drop",
+            duration: 500
+        }
+    });
+ 
+    $("#openerLyrics").click(function () {
+        $("#dialog").dialog("open");
+    });
+    
+    $("#openerRadio").click(function () {
+        $("#dialogRadio").dialog("open");
+    });
+    
+    $("#openerTemazoTortazo").click(function () {
+        if (temazoTortazoActivo == 0) {
+            $("#temazotortazo").html("<iframe src=\"https://www.lumicatch.com/viewer/YFyoMmfkGCh1643\" frameborder=\"false\" scrolling=\"false\" style=\"width: 846px; height: 510px; z-index: 20;position: fixed;top: 50%;left: 50%; -webkit-transform: translate(-50%, -50%); -moz-transform: translate(-50%, -50%); -o-transform: translate(-50%, -50%); transform: translate(-50%, -50%);\"></iframe>");
+            temazoTortazoActivo = 1;
+        } else {
+            $("#temazotortazo").html("");
+            temazoTortazoActivo = 0;
+        }
+    });
+     
+    $("body").on("click", ".ui-widget-overlay", function () {
+        $('#dialog').dialog("close");
+        $('#dialogRadio').dialog("close");
+    });
+});
