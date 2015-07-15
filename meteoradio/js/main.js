b0ee45d7a451d730b98594a4e241d5526408f7c7
Germán Cascales <germancascales@protonmail.ch>
2015-07-15 23:10:00 +0200

    JSONs MeteoRadio

--- meteoradio/js/main.js
+++ meteoradio/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var oTable = $('#myTable').dataTable({
-        "ajax": "data/json.txt",
+        "ajax": "data/malaga.txt",
         "columns": [
             { "data": "frecuencia", "sClass": "mdl-data-table__cell--non-numeric" },
             { "data": "asignacion", "sClass": "mdl-data-table__cell--non-numeric" },
@@ -27,17 +27,32 @@ $(document).ready(function() {
     
     var tab1 = document.getElementById('tab-1');
     tab1.addEventListener('click', function() {
-        oTable.api().ajax.url("data/json.txt").load();
+        oTable.api().ajax.url("data/malaga.txt").load();
     }, false);
     
     var tab2 = document.getElementById('tab-2');
     tab2.addEventListener('click', function() {
-        oTable.api().ajax.url("data/json1.txt").load();
+        oTable.api().ajax.url("data/fuengirola.txt").load();
     }, false);
     
     var tab3 = document.getElementById('tab-3');
     tab3.addEventListener('click', function() {
-        oTable.api().ajax.url("data/json.txt").load();
+        oTable.api().ajax.url("data/almayate.txt").load();
+    }, false);
+    
+    var tab4 = document.getElementById('tab-4');
+    tab4.addEventListener('click', function() {
+        oTable.api().ajax.url("data/antequera.txt").load();
+    }, false);
+    
+    var tab5 = document.getElementById('tab-5');
+    tab5.addEventListener('click', function() {
+        oTable.api().ajax.url("data/calahonda.txt").load();
+    }, false);
+    
+    var tab6 = document.getElementById('tab-6');
+    tab6.addEventListener('click', function() {
+        oTable.api().ajax.url("data/marbella.txt").load();
     }, false);
     
 } );
