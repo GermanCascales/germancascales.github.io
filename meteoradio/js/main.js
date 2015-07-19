4e726602efccff58d58b56a38e79b0064a5f77b4
GermanCascales <germancascales@gmail.com>
2015-07-19 20:23:08 +0200

    Actualización bandscans
    Añadir bandscan de Alhaurín de la Torre

--- meteoradio/js/main.js
+++ meteoradio/js/main.js
@@ -19,40 +19,46 @@ $(document).ready(function() {
                 "url": "http://cdn.datatables.net/plug-ins/1.10.7/i18n/Spanish.json"
             }
     });
+
     //frecuencia,asignacion,pty,text,pi,ps,confirmada,ubicacion,pir,grupo
 
     $('#search').keyup(function(){
          oTable.fnFilter($(this).val());
     })
-    
+
     var tab1 = document.getElementById('tab-1');
     tab1.addEventListener('click', function() {
         oTable.api().ajax.url("data/malaga.txt").load();
     }, false);
-    
+
     var tab2 = document.getElementById('tab-2');
     tab2.addEventListener('click', function() {
         oTable.api().ajax.url("data/fuengirola.txt").load();
     }, false);
-    
+
     var tab3 = document.getElementById('tab-3');
     tab3.addEventListener('click', function() {
         oTable.api().ajax.url("data/almayate.txt").load();
     }, false);
-    
+
     var tab4 = document.getElementById('tab-4');
     tab4.addEventListener('click', function() {
         oTable.api().ajax.url("data/antequera.txt").load();
     }, false);
-    
+
     var tab5 = document.getElementById('tab-5');
     tab5.addEventListener('click', function() {
         oTable.api().ajax.url("data/calahonda.txt").load();
     }, false);
-    
+
     var tab6 = document.getElementById('tab-6');
     tab6.addEventListener('click', function() {
         oTable.api().ajax.url("data/marbella.txt").load();
     }, false);
-    
+
+    var tab7 = document.getElementById('tab-7');
+    tab7.addEventListener('click', function() {
+        oTable.api().ajax.url("data/alhaurinelgrande.txt").load();
+    }, false);
+
 } );
